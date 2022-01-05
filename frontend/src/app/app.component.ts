111d9fb542c9bb1dd95fd958518961fc41e57b0e
MDmdr <rmd.melgar@gmail.com>
2022-01-05 11:18:48 -0600

    view dashboard ZT-sales v1

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sitiowebAii';
+}
+
+/* global bootstrap: false */
+/* mdr
+(function () {
+  'use strict'
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+*/
